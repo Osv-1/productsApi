f38caaa090cdb850a2d368bff0621afe023d9c08
Takeo <oswaldotakeo1@gmail.com>
2022-05-26 10:19:29 -0300

    Swagger Documentation

---
+++ src/main/java/com/br/productsapi/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.br.productsapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.ArrayList;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swagger() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        return new ApiInfo(
+                "Produtos API REST",
+                "API REST de cadastro de produtos.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Oswaldo Takeo", "https://www.linkedin.com/in/oswaldo-t-1a97a413a/",
+                        "oswaldotakeo1@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/licesen.html", new ArrayList<>()
+        );
+    }
+
+}
--- src/main/java/com/br/productsapi/controller/ProdutoController.java
+++ src/main/java/com/br/productsapi/controller/ProdutoController.java
@@ -2,6 +2,8 @@ package com.br.productsapi.controller;
 
 import com.br.productsapi.model.Produto;
 import com.br.productsapi.repository.ProdutoRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,28 +11,44 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/")
+@Api(value = "API REST PRODUCTS")
+@CrossOrigin(origins = "*")
 public class ProdutoController {
 
     @Autowired
     ProdutoRepository produtoRepository;
 
     @GetMapping("produtos")
-    public List<Produto> listaProdutos(){
-       return produtoRepository.findAll();
+    @ApiOperation(value = "RETURN LIST PRODUCT")
+    public List<Produto> listaProdutos() {
+        return produtoRepository.findAll();
 
     }
 
     @GetMapping("produtos/{id}")
-    public Produto listaProdutoUnico(@PathVariable(value = "id") long id){
+    @ApiOperation(value = "SEARCH FOR PRODUCT ID")
+    public Produto listaProdutoUnico(@PathVariable(value = "id") long id) {
         return produtoRepository.findById(id);
 
     }
 
     @PostMapping("produtos")
-    public Produto salvaProduto(@RequestBody Produto produto){
+    @ApiOperation(value = "SAVE PRODUCT")
+    public Produto salvaProduto(@RequestBody Produto produto) {
         return produtoRepository.save(produto);
     }
 
+    @DeleteMapping("produtos")
+    @ApiOperation(value = "DELETE PRODUCT ")
+    public void deletaProduto(@RequestBody Produto produto) {
+        produtoRepository.delete(produto);
+    }
+
+    @PutMapping("produtos")
+    @ApiOperation(value = "UPDATE PRODUCT ")
+    public Produto atualizaProduto(@RequestBody Produto produto) {
+        return produtoRepository.save(produto);
+    }
 
 
 }
